c878e2617fe6ab042a4d02a4979f8df7db5ef7ec
flenggo <justineberonio56@gmail.com>
2026-03-15 22:52:47 +0800

    added views and forms for the tasks

--- hangarin_projectsite/hangarin_projectsite/urls.py
+++ hangarin_projectsite/hangarin_projectsite/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from tasks.views import HomePageView, TaskListView
+from tasks.views import HomePageView, TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
-    path('tasks/', TaskListView.as_view(), name='task-list')
+    path('tasks/', TaskListView.as_view(), name='task-list'),
+    path('tasks/add/', TaskCreateView.as_view(), name='task-add'),
+    path('tasks/<pk>/', TaskUpdateView.as_view(), name='task-update'),
+    path('tasks/<pk>/delete/', TaskDeleteView.as_view(), name='task-delete'),
 ]
--- hangarin_projectsite/tasks/views.py
+++ hangarin_projectsite/tasks/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Task
 from django.urls import path
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.db.models import Q
+from .forms import TaskForm
 
 class HomePageView(ListView):
     model = Task
@@ -16,8 +20,42 @@ class HomePageView(ListView):
         context["pending_tasks"] = Task.objects.filter(status="Pending").count()
         return context
 
+
 class TaskListView(ListView):
     model = Task
     context_object_name = 'tasks'
     template_name = 'task_list.html'
     paginate_by = 5
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            qs = qs.filter(
+                Q(title__icontains=query) | Q(description__icontains=query)
+            )
+        return qs
+
+    def get_ordering(self):
+        allowed_sorts = ["title", "deadline", "status", "priority"]
+        sort_by = self.request.GET.get("sort_by")
+        if sort_by in allowed_sorts:
+            return sort_by
+        return "-created_at"
+
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'task_form.html'
+    success_url = reverse_lazy('task-list')
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'task_form.html' # We reuse the same form template for updating!
+    success_url = reverse_lazy('task-list')
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'task_del.html'
+    success_url = reverse_lazy('task-list')

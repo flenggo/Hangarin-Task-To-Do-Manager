df76998d6d926c07d540d4493e4363f9ccbce3a6
flenggo <justineberonio56@gmail.com>
2026-03-23 13:00:53 +0800

    added Login

--- hangarin_projectsite/hangarin_projectsite/urls.py
+++ hangarin_projectsite/hangarin_projectsite/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from tasks.views import HomePageView, TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView, CategoryListView, PriorityListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
+    path('accounts/', include('allauth.urls')),
     path('tasks/', TaskListView.as_view(), name='task-list'),
     path('tasks/add/', TaskCreateView.as_view(), name='task-add'),
     path('tasks/<pk>/', TaskUpdateView.as_view(), name='task-update'),
